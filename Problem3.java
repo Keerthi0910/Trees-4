52e5539f72aad506138a93b337a4a5e5d8d8f9fb
Keerthi0910 <chundurikeerthipriya@gmail.com>
2026-09-19 23:30:55 -0500

    Add lowestCommonAncestor method for binary tree
    Implement lowestCommonAncestor method to find LCA of two nodes in a binary tree.

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,64 @@
+o(n) time complexity
+  o(n) space complexity
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<TreeNode>  pathP;
+    List<TreeNode>  pathQ;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        pathP = new ArrayList<>();
+          pathQ = new ArrayList<>();
+
+        helper(root, p , q, new ArrayList<>());
+
+        TreeNode result = null; 
+
+        for(int i = 0 ; i < Math.min(pathP.size() , pathQ.size()); i++){
+            if(pathP.get(i).val == pathQ.get(i).val){
+                result = pathP.get(i);
+            } else {
+                break;
+            }
+
+        }
+        return result;
+       
+      
+
+        
+    }
+
+    private void helper(TreeNode root, TreeNode p , TreeNode q, List<TreeNode> path) {
+        if(root == null){
+             return;
+        }
+        path.add(root);
+
+        helper(root.left, p  ,q, path);
+
+        if(root == p ){
+            pathP.addAll(path);
+            pathP.add(root);
+
+        }
+        
+        if(root == q ){
+        
+            pathQ.addAll(path);
+            pathQ.add(root);
+        }
+
+        helper(root.right, p, q, path);
+         path.remove(path.size() - 1);
+
+
+    }
+}
